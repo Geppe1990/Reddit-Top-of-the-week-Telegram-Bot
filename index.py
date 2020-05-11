fba03fa3ecc327b1cf4b27fdffa2a670d2003784
Giuseppe Vigneri <geppe1990@gmail.com>
2020-05-11 21:24:08 +0200

    Telegram Management

--- index.py
+++ index.py
@@ -1,22 +1,38 @@
 import praw
-import pandas as pd
 import datetime as dt
+import botogram
 
 
 def main():
     data = {}
+
     with open("data.txt") as f:
         for line in f:
-            key, value = line.strip().split(':')
+            key, value = line.strip().split(': ')
             data[key] = value
 
     reddit = praw.Reddit(
-        client_id=data["client_id"],
-        client_secret=data["client_secret"],
-        user_agent=data["user_agent"],
-        username=data["username"],
-        password=data["password"]
+        client_id=data["reddit_client_id"].strip(),
+        client_secret=data["reddit_client_secret"].strip(),
+        user_agent=data["reddit_user_agent"].strip(),
+        username=data["reddit_username"].strip(),
+        password=data["reddit_password"].strip()
     )
+
+    bot = botogram.create(data["telegram_token"])
+
+    hot_limit = 5
+    subreddit_list = ['redditdev', 'Nootropics']
+    for sr in subreddit_list:
+        subreddit = reddit.subreddit(sr)
+        message = "<b>"+sr.upper()+"</b>\n"
+        # Mi ricavo i post hot che non sono stickied
+        top_ten = [p for p in subreddit.hot(limit=100) if not p.stickied][:hot_limit]
+
+        for post in top_ten:
+            message += "<a href='"+post.url+"'>"+post.title+"</a>\n<i>"+str(dt.datetime.fromtimestamp(post.created))+"</i>\n\n"
+
+        bot.chat(data["telegram_id"]).send(message)
 
 
 if __name__ == '__main__':
